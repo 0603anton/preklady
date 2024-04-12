82be287697e298fd821e6fbc9d9e8cbf81b98b51
0603anton <zorgzb@gmail.com>
2024-04-12 10:03:29 +0200

    7 commit main site done - footer add

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@ $(document).ready(function(){
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             }
             // You can unslick at a given breakpoint now by adding:
